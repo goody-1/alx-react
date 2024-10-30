d0ec50948042f9dc10390b7f08226a242ad23e12
Atunde Goodness <atundeg@gmail.com>
2024-10-30 19:16:35 +0000

    remove code not needed

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'production',
@@ -7,7 +6,6 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'public'),
-        clean: true,
     },
     module: {
         rules: [
@@ -17,16 +15,13 @@ module.exports = {
             },
             {
                 test: /\.(png|jpe?g|gif)$/i, // Image loader
-                type: 'asset/resource',
-                generator: {
-                    filename: 'images/[name][ext]', // Output images to images/ folder
-                },
+                type: 'asset',
                 use: [
                     {
                         loader: 'image-webpack-loader', // Optimize images
                         options: {
                             disable: process.env.NODE_ENV !== 'production', // Disable in dev mode
-                            mozjpeg: { progressive: true },
+                            mozjpeg: { progressive: true, quality: 65 },
                             optipng: { enabled: false },
                             pngquant: { quality: [0.65, 0.90], speed: 4 },
                             gifsicle: { interlaced: false },
@@ -36,10 +31,8 @@ module.exports = {
             },
         ],
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'public/index.html'), // Template HTML
-            filename: 'index.html', // Output HTML name
-        }),
-    ],
+    performance: {
+        maxAssetSize: 1000000,
+        maxEntrypointSize: 1000000,
+      },
 };
