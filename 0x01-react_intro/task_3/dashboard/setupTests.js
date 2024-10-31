31bba4b8c064360d51853199e8d25b3c1c06207c
Atunde Goodness <atundeg@gmail.com>
2024-10-31 00:27:20 +0000

    add tests

--- 0x01-react_intro/task_3/dashboard/setupTests.js
+++ 0x01-react_intro/task_3/dashboard/setupTests.js
@@ -3,3 +3,9 @@ import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
  
 Enzyme.configure({ adapter: new Adapter() });
+
+// src/setupTests.js
+
+global.TextEncoder = require('util').TextEncoder;
+global.TextDecoder = require('util').TextDecoder;
+
