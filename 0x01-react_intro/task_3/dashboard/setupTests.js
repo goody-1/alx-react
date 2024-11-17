a95bce4e30e552b8631bb9d35052eb59fce93224
Atunde Goodness <atundeg@gmail.com>
2024-11-17 12:48:59 +0000

    do task

--- 0x01-react_intro/task_3/dashboard/setupTests.js
+++ 0x01-react_intro/task_3/dashboard/setupTests.js
@@ -1,10 +1,10 @@
-import '@testing-library/jest-dom';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
- 
+
 Enzyme.configure({ adapter: new Adapter() });
 
 // src/setupTests.js
+import '@testing-library/jest-dom';
 
 global.TextEncoder = require('util').TextEncoder;
 global.TextDecoder = require('util').TextDecoder;
